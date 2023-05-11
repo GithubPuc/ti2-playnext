46507a071237bb04083d8b816417784018b40820
GithubPuc <1445697@sga.pucminas.br>
2023-05-11 18:05:19 -0300

    Fixed indentation

--- src/main/java/service/JogoService.java
+++ src/main/java/service/JogoService.java
@@ -12,112 +12,112 @@ import spark.Request;
 import spark.Response;
 
 public class JogoService {
-    private static final ObjectMapper objectMapper = new ObjectMapper();
-    private JogoDAO dao = new JogoDAO();
-    private boolean statusDAO;
+	private static final ObjectMapper objectMapper = new ObjectMapper();
+	private JogoDAO dao = new JogoDAO();
+	private boolean statusDAO;
 
-    public JogoService() {
-        statusDAO = dao.conectar();
-    }
+	public JogoService() {
+		statusDAO = dao.conectar();
+	}
 
-    private String makeIndex() {
-        String html = "";
-        try {
-            Scanner sc = new Scanner(new File("src/main/resources/internal/indexJogo.html"));
-            while (sc.hasNext()) {
-                html += sc.nextLine() + '\n';
-            }
-            sc.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        html = html.replaceFirst("~ERRO~", statusDAO ? "\"\"" : "Erro de conexão com banco de dados");
-        return html;
-    }
+	private String makeIndex() {
+		String html = "";
+		try {
+			Scanner sc = new Scanner(new File("src/main/resources/internal/indexJogo.html"));
+			while (sc.hasNext()) {
+				html += sc.nextLine() + '\n';
+			}
+			sc.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		html = html.replaceFirst("~ERRO~", statusDAO ? "\"\"" : "Erro de conexão com banco de dados");
+		return html;
+	}
 
-    public Object getListar(Request request, Response response) {
-        String html = makeIndex();
-        html = html.replaceFirst("~FETCH~", "/jogos");
-        return html;
-    }
+	public Object getListar(Request request, Response response) {
+		String html = makeIndex();
+		html = html.replaceFirst("~FETCH~", "/jogos");
+		return html;
+	}
 
-    public Object getLerJogo(Request request, Response response) {
-        String html = makeIndex();
-        html = html.replaceFirst("~FETCH~", "/jogo");
-        html = html.replaceFirst("~FBODY~", "{\"idJogo\":\"" + request.params(":idJogo") + "\"}");
-        return html;
-    }
+	public Object getLerJogo(Request request, Response response) {
+		String html = makeIndex();
+		html = html.replaceFirst("~FETCH~", "/jogo");
+		html = html.replaceFirst("~FBODY~", "{\"idJogo\":\"" + request.params(":idJogo") + "\"}");
+		return html;
+	}
 
-    public Object postListar(Request request, Response response) {
-        response.type("application/json");
-        return jsonLista(dao.listarJogos());
-    }
+	public Object postListar(Request request, Response response) {
+		response.type("application/json");
+		return jsonLista(dao.listarJogos());
+	}
 
-    public Object postLerJogo(Request request, Response response) {
-        response.type("application/json");
-        try {
-            JsonNode parent = objectMapper.readTree(request.body());
-            Long idJogo = parent.path("idJogo").asLong();
-            if (idJogo == 0L)
-                return jsonLista(dao.listarJogos());
-            return jsonPadrao(dao.lerJogo(idJogo));
-        } catch (Exception e) {
-            response.status(400);
-            return jsonPadrao("\"BAD REQUEST\"");
-        }
-    }
+	public Object postLerJogo(Request request, Response response) {
+		response.type("application/json");
+		try {
+			JsonNode parent = objectMapper.readTree(request.body());
+			Long idJogo = parent.path("idJogo").asLong();
+			if (idJogo == 0L)
+				return jsonLista(dao.listarJogos());
+			return jsonPadrao(dao.lerJogo(idJogo));
+		} catch (Exception e) {
+			response.status(400);
+			return jsonPadrao("\"BAD REQUEST\"");
+		}
+	}
 
-    public Object postCriarJogo(Request request, Response response) {
-        response.type("application/json");
-        try {
-            Jogo u = objectMapper.readValue(request.body(), Jogo.class);
-            return jsonPadrao(dao.inserirJogo(u) ? "Sucesso" : "Erro interno");
-        } catch (Exception e) {
-            response.status(400);
-            return jsonPadrao("\"BAD REQUEST\"");
-        }
-    }
+	public Object postCriarJogo(Request request, Response response) {
+		response.type("application/json");
+		try {
+			Jogo u = objectMapper.readValue(request.body(), Jogo.class);
+			return jsonPadrao(dao.inserirJogo(u) ? "Sucesso" : "Erro interno");
+		} catch (Exception e) {
+			response.status(400);
+			return jsonPadrao("\"BAD REQUEST\"");
+		}
+	}
 
-    public Object postAtualizarJogo(Request request, Response response) {
-        response.type("application/json");
-        try {
-            Jogo u = objectMapper.readValue(request.body(), Jogo.class);
-            return jsonPadrao(dao.atualizarJogo(u) ? "Sucesso" : "Erro interno");
-        } catch (Exception e) {
-            response.status(400);
-            return jsonPadrao("\"BAD REQUEST\"");
-        }
-    }
+	public Object postAtualizarJogo(Request request, Response response) {
+		response.type("application/json");
+		try {
+			Jogo u = objectMapper.readValue(request.body(), Jogo.class);
+			return jsonPadrao(dao.atualizarJogo(u) ? "Sucesso" : "Erro interno");
+		} catch (Exception e) {
+			response.status(400);
+			return jsonPadrao("\"BAD REQUEST\"");
+		}
+	}
 
-    public Object postDeletarJogo(Request request, Response response) {
-        response.type("application/json");
-        try {
-            JsonNode parent = objectMapper.readTree(request.body());
-            Long idJogo = parent.path("idJogo").asLong();
-            return jsonPadrao(0, dao.excluirJogo(idJogo) ? "Excluido" : "Erro interno");
-        } catch (Exception e) {
-            response.status(400);
-            return jsonPadrao("\"BAD REQUEST\"");
-        }
-    }
+	public Object postDeletarJogo(Request request, Response response) {
+		response.type("application/json");
+		try {
+			JsonNode parent = objectMapper.readTree(request.body());
+			Long idJogo = parent.path("idJogo").asLong();
+			return jsonPadrao(0, dao.excluirJogo(idJogo) ? "Excluido" : "Erro interno");
+		} catch (Exception e) {
+			response.status(400);
+			return jsonPadrao("\"BAD REQUEST\"");
+		}
+	}
 
-    private String jsonPadrao(int tipo, String valor) {
-        return String.format("{\"tipo\":%d,\"valor\":%s}", tipo, valor);
-    }
+	private String jsonPadrao(int tipo, String valor) {
+		return String.format("{\"tipo\":%d,\"valor\":%s}", tipo, valor);
+	}
 
-    private String jsonLista(Object[] o) {
-        try {
-            return jsonPadrao(1, objectMapper.writeValueAsString(o));
-        } catch (Exception e) {
-            return jsonPadrao(0, "Erro interno");
-        }
-    }
+	private String jsonLista(Object[] o) {
+		try {
+			return jsonPadrao(1, objectMapper.writeValueAsString(o));
+		} catch (Exception e) {
+			return jsonPadrao(0, "Erro interno");
+		}
+	}
 
-    private String jsonPadrao(Object o) {
-        try {
-            return jsonPadrao(0, objectMapper.writeValueAsString(o));
-        } catch (Exception e) {
-            return jsonPadrao(0, "Erro interno");
-        }
-    }
+	private String jsonPadrao(Object o) {
+		try {
+			return jsonPadrao(0, objectMapper.writeValueAsString(o));
+		} catch (Exception e) {
+			return jsonPadrao(0, "Erro interno");
+		}
+	}
 }
--- src/main/java/service/UsuarioService.java
+++ src/main/java/service/UsuarioService.java
@@ -12,112 +12,112 @@ import spark.Request;
 import spark.Response;
 
 public class UsuarioService {
-    private static final ObjectMapper objectMapper = new ObjectMapper();
-    private UsuarioDAO dao = new UsuarioDAO();
-    private boolean statusDAO;
+	private static final ObjectMapper objectMapper = new ObjectMapper();
+	private UsuarioDAO dao = new UsuarioDAO();
+	private boolean statusDAO;
 
-    public UsuarioService() {
-        statusDAO = dao.conectar();
-    }
+	public UsuarioService() {
+		statusDAO = dao.conectar();
+	}
 
-    private String makeIndex() {
-        String html = "";
-        try {
-            Scanner sc = new Scanner(new File("src/main/resources/internal/indexUsuario.html"));
-            while (sc.hasNext()) {
-                html += sc.nextLine() + '\n';
-            }
-            sc.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        html = html.replaceFirst("~ERRO~", statusDAO ? "\"\"" : "Erro de conexão com banco de dados");
-        return html;
-    }
+	private String makeIndex() {
+		String html = "";
+		try {
+			Scanner sc = new Scanner(new File("src/main/resources/internal/indexUsuario.html"));
+			while (sc.hasNext()) {
+				html += sc.nextLine() + '\n';
+			}
+			sc.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		html = html.replaceFirst("~ERRO~", statusDAO ? "\"\"" : "Erro de conexão com banco de dados");
+		return html;
+	}
 
-    public Object getListar(Request request, Response response) {
-        String html = makeIndex();
-        html = html.replaceFirst("~FETCH~", "/usuarios");
-        return html;
-    }
+	public Object getListar(Request request, Response response) {
+		String html = makeIndex();
+		html = html.replaceFirst("~FETCH~", "/usuarios");
+		return html;
+	}
 
-    public Object postListar(Request request, Response response) {
-        response.type("application/json");
-        return jsonLista(dao.listarUsuarios());
-    }
+	public Object postListar(Request request, Response response) {
+		response.type("application/json");
+		return jsonLista(dao.listarUsuarios());
+	}
 
-    public Object getLerUsuario(Request request, Response response) {
-        String html = makeIndex();
-        html = html.replaceFirst("~FETCH~", "/Usuario");
-        html = html.replaceFirst("~FBODY~", "{\"idUsuario\":\"" + request.params(":idUsuario") + "\"}");
-        return html;
-    }
+	public Object getLerUsuario(Request request, Response response) {
+		String html = makeIndex();
+		html = html.replaceFirst("~FETCH~", "/Usuario");
+		html = html.replaceFirst("~FBODY~", "{\"idUsuario\":\"" + request.params(":idUsuario") + "\"}");
+		return html;
+	}
 
-    public Object postLerUsuario(Request request, Response response) {
-        response.type("application/json");
-        try {
-            JsonNode parent = objectMapper.readTree(request.body());
-            Long idUsuario = parent.path("idUsuario").asLong();
-            if (idUsuario == 0L)
-                return jsonLista(dao.listarUsuarios());
-            return jsonPadrao(dao.lerUsuario(idUsuario));
-        } catch (Exception e) {
-            response.status(400);
-            return jsonPadrao("\"BAD REQUEST\"");
-        }
-    }
+	public Object postLerUsuario(Request request, Response response) {
+		response.type("application/json");
+		try {
+			JsonNode parent = objectMapper.readTree(request.body());
+			Long idUsuario = parent.path("idUsuario").asLong();
+			if (idUsuario == 0L)
+				return jsonLista(dao.listarUsuarios());
+			return jsonPadrao(dao.lerUsuario(idUsuario));
+		} catch (Exception e) {
+			response.status(400);
+			return jsonPadrao("\"BAD REQUEST\"");
+		}
+	}
 
-    public Object postCriarUsuario(Request request, Response response) {
-        response.type("application/json");
-        try {
-            Usuario u = objectMapper.readValue(request.body(), Usuario.class);
-            return jsonPadrao(dao.inserirUsuario(u) ? "Sucesso" : "Erro interno");
-        } catch (Exception e) {
-            response.status(400);
-            return jsonPadrao("\"BAD REQUEST\"");
-        }
-    }
+	public Object postCriarUsuario(Request request, Response response) {
+		response.type("application/json");
+		try {
+			Usuario u = objectMapper.readValue(request.body(), Usuario.class);
+			return jsonPadrao(dao.inserirUsuario(u) ? "Sucesso" : "Erro interno");
+		} catch (Exception e) {
+			response.status(400);
+			return jsonPadrao("\"BAD REQUEST\"");
+		}
+	}
 
-    public Object postAtualizarUsuario(Request request, Response response) {
-        response.type("application/json");
-        try {
-            Usuario u = objectMapper.readValue(request.body(), Usuario.class);
-            return jsonPadrao(dao.atualizarUsuario(u) ? "Sucesso" : "Erro interno");
-        } catch (Exception e) {
-            response.status(400);
-            return jsonPadrao("\"BAD REQUEST\"");
-        }
-    }
+	public Object postAtualizarUsuario(Request request, Response response) {
+		response.type("application/json");
+		try {
+			Usuario u = objectMapper.readValue(request.body(), Usuario.class);
+			return jsonPadrao(dao.atualizarUsuario(u) ? "Sucesso" : "Erro interno");
+		} catch (Exception e) {
+			response.status(400);
+			return jsonPadrao("\"BAD REQUEST\"");
+		}
+	}
 
-    public Object postDeletarUsuario(Request request, Response response) {
-        response.type("application/json");
-        try {
-            JsonNode parent = objectMapper.readTree(request.body());
-            Long idUsuario = parent.path("idUsuario").asLong();
-            return jsonPadrao(0, dao.excluirUsuario(idUsuario) ? "Excluido" : "Erro interno");
-        } catch (Exception e) {
-            response.status(400);
-            return jsonPadrao("\"BAD REQUEST\"");
-        }
-    }
+	public Object postDeletarUsuario(Request request, Response response) {
+		response.type("application/json");
+		try {
+			JsonNode parent = objectMapper.readTree(request.body());
+			Long idUsuario = parent.path("idUsuario").asLong();
+			return jsonPadrao(0, dao.excluirUsuario(idUsuario) ? "Excluido" : "Erro interno");
+		} catch (Exception e) {
+			response.status(400);
+			return jsonPadrao("\"BAD REQUEST\"");
+		}
+	}
 
-    private String jsonPadrao(int tipo, String valor) {
-        return String.format("{\"tipo\":%d,\"valor\":%s}", tipo, valor);
-    }
+	private String jsonPadrao(int tipo, String valor) {
+		return String.format("{\"tipo\":%d,\"valor\":%s}", tipo, valor);
+	}
 
-    private String jsonLista(Object[] o) {
-        try {
-            return jsonPadrao(1, objectMapper.writeValueAsString(o));
-        } catch (Exception e) {
-            return jsonPadrao(0, "Erro interno");
-        }
-    }
+	private String jsonLista(Object[] o) {
+		try {
+			return jsonPadrao(1, objectMapper.writeValueAsString(o));
+		} catch (Exception e) {
+			return jsonPadrao(0, "Erro interno");
+		}
+	}
 
-    private String jsonPadrao(Object o) {
-        try {
-            return jsonPadrao(0, objectMapper.writeValueAsString(o));
-        } catch (Exception e) {
-            return jsonPadrao(0, "Erro Interno");
-        }
-    }
+	private String jsonPadrao(Object o) {
+		try {
+			return jsonPadrao(0, objectMapper.writeValueAsString(o));
+		} catch (Exception e) {
+			return jsonPadrao(0, "Erro Interno");
+		}
+	}
 }
